05e03b9f5ec18bbfc29f10b46e9aa44fd234ed66
Brandon Trussell <brandonwtrussell@gmail.com>
2023-09-23 11:56:02 -0400

    Initial setup

--- public/config/shared.lua
+++ public/config/shared.lua
@@ -1,5 +1,5 @@
 shConfig = {
     -- Set your framework that you're using. Possible values: 'ESX', 'QB', 'STANDALONE', 'CUSTOM'
     -- When using 'CUSTOM', you have to fill the functions yourself in /public/framework/server.lua, most importantly the getPlayerIdentifier function
-    framework = 'STANDALONE',
+    framework = 'QB',
 }
